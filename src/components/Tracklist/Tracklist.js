4fd7c8571ba7b34c83995072f1cdf0384a59753a
Remi Guillou <beaugossremi@gmail.com>
2021-04-22 19:09:50 +0200

    components css and html

---
+++ src/components/Tracklist/Tracklist.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Track from "../Track/Track.js"
+import "./Tracklist.css"
+
+class Tracklist extends React.Component {
+
+
+    render() {
+
+        return(
+            <div class="TrackList">
+                
+                <h1>//You will add a map method that renders a set of Track components</h1>
+            </div>
+        )
+    }
+}
+
+export default Tracklist;
